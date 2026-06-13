98883c98b68d75e9912c0cb5ee09da8de0a1218d
ganeshtech <ganeshtech2017@gmail.com>
2026-06-13 23:01:15 +0530

    fixing apirul issue

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://ganeshtech2017.runasp.net/api';
+export const API_BASE_URL = 'https://ganeshtech2017.runasp.net/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
